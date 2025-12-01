65d04a7e3dbb0784d3e667acee7a3b36d953c9f8
KALIMUTHU S <KALIMUTHU7SKM@GMAIL.COM>
2025-12-01 10:39:57 +0530

    fix some errors

--- src/main/java/com/skm/redditclone/model/User.java
+++ src/main/java/com/skm/redditclone/model/User.java
@@ -12,7 +12,7 @@ import java.time.Instant;
 public class User {
     private Long id;
     private String username;
-    private String password; // hashed
+    private String password;
     private Instant createdAt;
 }
 
